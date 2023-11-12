b59f9bf42337c0e848855650509003afacf5cf73
Maxime Bressange <maxime.bressange@gmail.com>
2023-11-12 17:32:02 +0100

    Add images and responsive

--- src/app/components/Box.tsx
+++ src/app/components/Box.tsx
@@ -2,24 +2,60 @@ import Image from 'next/image';
 
 type BoxProps = BoxItem & {
   index: number;
-  onClick: () => void;
+  onRightAnswer: () => void;
+    onWrongAnswer: () => void;
 }
 
-const Box = ({ index, firstname, illustration, onClick }: BoxProps) => {
+const Box = ({ index, firstname, image, illustration, onRightAnswer, onWrongAnswer }: BoxProps) => {
+    const options = [
+        'Nicolas et Alexie',
+        'Camille',
+        'Julie',
+        'Mathieu',
+        'Mehdi',
+        'Pauline',
+        'Pierrick',
+        'Manon',
+        'Sabrina',
+        'Maxime',
+        'Alexie',
+    ]
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formJson = Object.fromEntries(formData.entries());
+
+        if(formJson.friends === firstname) {
+            onRightAnswer();
+        } else {
+            onWrongAnswer();
+        }
+    }
+
   return (
     <div className={`day day-${index + 1}`}>
         <label>
             <input type="checkbox"/>
             <div className="door">
-                <div className="front">{index + 1}</div>
+                <div className="front bg-white">
+                    <Image src={illustration} alt={firstname} width={200} height={200} />
+                </div>
                 <div className="back"></div>
             </div>
-            <div className="inside bg-white">
-                <Image src={illustration} alt={firstname} width={200} height={200} />
+            <div className="inside bg-gray-600 flex justify-center align-center">
+                <div className="w-full relative">
+                    <Image src={image} alt={firstname} width={250} height={250} className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2" />
+                </div>
             </div>
-            <button className="title-container" onClick={onClick}>
-                <span>Découvrir</span>
-            </button>
+            <form method="post" onSubmit={handleSubmit} className="title-container flex flex-col gap-4 absolute left-1/2 -translate-x-1/2">
+                <select name={`friends`}>
+                    {options.map((option, index) => {
+                        return <option key={index} value={option}>{option}</option>
+                    })}
+                </select>
+                <button type="submit">Découvrir</button>
+            </form>
         </label>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,67 +6,74 @@ import Modal from "react-modal";
 export default function Home() {
   const [boxSelected, setBoxSelected] = useState<BoxItem|undefined>();
   const [modalOpened, setModalOpened] = useState<boolean>(false);
+  const [wrongModalOpened, setWrongModalOpened] = useState<boolean>(false);
 
   const boxItems: BoxItem[] = [
     {
       firstname: 'Nicolas et Alexie',
-      image: '/img/picture.png',
-      illustration: '/img/baldak-alexie.png',
+      image: '/img/picture/baldak-alexie.jpeg',
+      illustration: '/img/illustration/baldak-alexie.png',
       video: '/video/baldak-alexie.mp4',
     },
     {
       firstname: 'Camille',
-      image: '/img/picture.png',
-      illustration: '/img/camille.png',
+      image: '/img/picture/camille.jpg',
+      illustration: '/img/illustration/camille.png',
       video: '/video/camille.mp4',
     },
     {
       firstname: 'Julie',
-      image: '/img/picture.png',
-      illustration: '/img/julie.png',
+      image: '/img/picture/julie.png',
+      illustration: '/img/illustration/julie.png',
       video: '/video/julie.mp4',
     },
     {
-      firstname: 'Mathieu',
-      image: '/img/picture.png',
-      illustration: '/img/mathieu.png',
-      video: '/video/mathieu.mp4',
+      firstname: 'Maxime',
+      image: '/img/picture/maxime.jpeg',
+      illustration: '/img/illustration/maxime.png',
+      video: '/video/maxime.mp4',
     },
     {
       firstname: 'Mehdi',
-      image: '/img/picture.png',
-      illustration: '/img/mehdi.png',
+      image: '/img/picture/mehdi.jpg',
+      illustration: '/img/illustration/mehdi.png',
       video: '/video/mehdi.mp4',
     },
     {
       firstname: 'Pauline',
-      image: '/img/picture.png',
-      illustration: '/img/pauline.png',
+      image: '/img/picture/pauline.jpg',
+      illustration: '/img/illustration/pauline.png',
       video: '/video/pauline.mp4',
     },
     {
       firstname: 'Pierrick',
-      image: '/img/picture.png',
-      illustration: '/img/pierrick.png',
+      image: '/img/picture/pierrick.jpeg',
+      illustration: '/img/illustration/pierrick.png',
       video: '/video/pierrick.mp4',
     },
     {
       firstname: 'Manon',
-      image: '/img/picture.png',
-      illustration: '/img/manon.png',
+      image: '/img/picture/manon.jpg',
+      illustration: '/img/illustration/manon.png',
       video: '/video/manon.mp4',
     },
     {
       firstname: 'Sabrina',
-      image: '/img/picture.png',
-      illustration: '/img/sabrina.png',
+      image: '/img/picture/sabrina.jpg',
+      illustration: '/img/illustration/sabrina.png',
       video: '/video/sabrina.mp4',
     },
     {
-      firstname: 'Maxime',
-      image: '/img/picture.png',
-      illustration: '/img/maxime.png',
-      video: '/video/maxime.mp4',
+      firstname: 'Mathieu',
+      image: '/img/picture/mathieu.jpg',
+      illustration: '/img/illustration/mathieu.png',
+      video: '/video/mathieu.mp4',
+    },
+    {
+      firstname: 'Alexie',
+      image: '/img/picture/alexie.jpg',
+      illustration: '/img/illustration/alexie.png',
+      video: '/video/alexie.jpg',
     }
   ]
 
@@ -78,9 +85,22 @@ export default function Home() {
   return (
     <>
       <div className="calendar-grid">
-        <h1 className="title flex justify-center items-center text-4xl text-cyan-700">Le jeu du calendrier des amis</h1>
+        <div className="title text-red-600">
+          <h1>
+            Le jeu du calendrier des amis ⛄
+          </h1>
+          <h2 className="text-xl">
+            Derrière chaque case se cache un ami ! Ouvre et découvre un indice pour voir qui se cache derrière 😉
+          </h2>
+        </div>
           {boxItems.map((item, index) => (
-            <Box key={index} {...item} index={index} onClick={() => onBoxClicked(item)} />
+            <Box
+              key={index}
+              index={index}
+              onRightAnswer={() => onBoxClicked(item)}
+              onWrongAnswer={() => setWrongModalOpened(true)}
+              {...item}
+            />
           ))}
       </div>
       {modalOpened &&
@@ -94,12 +114,35 @@ export default function Home() {
               onClick={() => setModalOpened(false)}
           >
             <svg width="20" height="20" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-              <line x1="0" y1="100" x2="100" y2="0" stroke-width="5" stroke="black"/>
-              <line x1="0" y1="0" x2="100" y2="100" stroke-width="5" stroke="black"/>
+              <line x1="0" y1="100" x2="100" y2="0" strokeWidth="5" stroke="black"/>
+              <line x1="0" y1="0" x2="100" y2="100" strokeWidth="5" stroke="black"/>
             </svg>
           </button>
-          {boxSelected && <VideoPlayer url={boxSelected.video}/>}
+          {boxSelected?.firstname === 'Alexie' ?
+            <Image src={boxSelected.video} alt={boxSelected.firstname} width={500} height={500}/>
+            : boxSelected && <VideoPlayer url={boxSelected.video}/>
+          }
         </Modal>
+      }
+      {wrongModalOpened &&
+          <Modal
+              className="h-screen flex"
+              isOpen={wrongModalOpened}
+              onRequestClose={() => setWrongModalOpened(false)}
+          >
+            <button
+                className="absolute right-10 top-10"
+                onClick={() => setWrongModalOpened(false)}
+            >
+              <svg width="20" height="20" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <line x1="0" y1="100" x2="100" y2="0" strokeWidth="5" stroke="black"/>
+                <line x1="0" y1="0" x2="100" y2="100" strokeWidth="5" stroke="black"/>
+              </svg>
+            </button>
+            <div className="relative w-full">
+              <Image src="/img/picture/c-est-non.gif" alt="c'est non" width={300} height={300} className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"/>
+            </div>
+          </Modal>
       }
     </>
   )
@@ -107,3 +150,4 @@ export default function Home() {
 
 import { Box } from '@/app/components/Box';
 import {VideoPlayer} from "@/app/components/VideoPlayer";
+import Image from "next/image";
